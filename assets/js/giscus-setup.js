b828564387fecd169a4d01cf485c72d06d73767c
CGaffey <42742102+CGaffey@users.noreply.github.com>
2025-12-08 02:27:41 +0000

    Deploying to gh-pages from @ CGaffey/cgaffey.github.io@840ee25f6b06ad20eb4a765d8497926b88ed0034 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "CGaffey/claregaffey.github.io",
+    "data-repo": "CGaffey/cgaffey.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
